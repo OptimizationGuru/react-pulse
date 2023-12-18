03993e42915ed9a37381a3c94e86317e23426b4f
Shivam Tiwari <shivam.tiwari@yulu.bike>
2023-12-18 22:48:24 +0530

    episde-04

--- App.js
+++ App.js
@@ -2,66 +2,77 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement('h1', {id:"heading", xyz:'abc'}, "Hello World Using React!")
 const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading) // it replaces the existing DOM inside root
-console.log(heading)  // React.createElement returns an object aka React Element, not HTML element
+/*
+* Building Food Ordering App - QuickCrave
+* Header - Logo, Nav-Items, Cart
+* Body - Search, Restaurant Container -Cards inside - img, Star Rating, cuisine  delivery time etc
+* Footer - copyright, links, contact, address, info
+*/
 
-// React.createElement => ReactElement(JS object) => HTML Element on rendering
-
-// creation of Nested Elements
-
-const parent = React.createElement('div', {id:'parent'},
-
-React.createElement('div', {id:'child'},
-
-React.createElement('h1', {id:"heading"}, 'Namaste Bharat 🇮🇳 🙏 🙏')))
- 
-// root.render(parent)
-
-console.log(parent)
-
-
-// If you want to make more nested structure, the code will become messy
-// JSX comes to rescue for writing clean code
-// JSX is a js syntax which makes it easier to create React element
-// JSX is not HTML inside JS, rather it is HTML like syntax
-// React can be written w/o JSX for sure
-// JSX is not React
-// JSX is not a valid javascript, PARCEL is transpiling/converting jsx into pure js which js engine can understand
-// Parcel is having Babel inside ==> BABEL transpiles jsx into pure js so that browser can understand it
-// Babel is a opensource JS compiler, read about it
-// If you write JSX in multiple lines, you must use brackets(), babel gets to know where jsx starts and end
-
-// JSX =>Babel transpiles it to  React.createElement => React.createElement => JS obejct => HTML Element(on rendering)
-// JSX internally uses Recat.createElement only
-const jsxHeading = (<h1 id="heading">Namaste Bharat 🇮🇳  🙏🙏 </h1>)  // it is a react element
-const shoot = ReactDOM.createRoot(document.getElementById('shoot'))
-console.log( jsxHeading, 'jsxheading')
-shoot.render(jsxHeading)
+const Header = () => {
+ return (
+    <div className="header">
+        <div>
+            <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRmaaj1dqXMUKLbNAjVKYq6OhtCmAuWEKIjg&usqp=CAU"/>
+        </div>
+        <div className="nav-items">
+        <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+        </ul>
+        </div>
+    </div>
+ )
+};
 
 
-// React Component
-// 1) Class based Components - OLD
-// 2)Functional Components - NEW
+//JS object for giving inline style
+const styleCard = {
+    backgroundColor : "#f0f0f0",
+};
 
-// React Functional Component ==> a simple js function ==> which returns some JSX code/element
-const SubHeading =()=> (<div className="subheading">I am a subheading</div>)
-const HeadingComponent = ()=>(
-    <div id="container">
-     <h1 className="header">Namaste Bharat 🇮🇳 Functional Component</h1>
-     <SubHeading/>
-     <SubHeading></SubHeading>
-     {SubHeading()}
-     {/* line 53 54 and 55 are all same, a function can also be called inside jsx */}
-     {console.log('Hii')}
-     {jsxHeading}
-     </div>
-)
-// The nesting of compoents above is c/a Component Composition
-// One can write javscript inside JSX => very powerful thing
+const RestaurantCard =(props)=>{
+ console.log(props)
+ const {resName, cuisine} = props
+    return(
+        <div className="res-card" style={styleCard}>
+           <img className="res-logo"  src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/59b64f9bb67234987652437f21d379c2"/>
+           <div className="res-desc">
+            <h2>{resName}</h2>
+            <h3> {cuisine}</h3>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+            </div>
 
-// How to put react element inside jsx
-// Read about how jsx sanitise data from apicalls to prevent malaicious data from apis
-root.render(<HeadingComponent/>)
-// Babel distinguish b/w React Element and React functional componet by the manner they are rendered
+        </div>
+    )
+}
+
+const Body =()=>{
+    return(
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+               <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
+               <RestaurantCard resName="Oven Story" cuisine="Pizza, Italian"/>
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+   return (
+    <div className="app">
+   <Header/>
+   <Body/>
+    </div>
+   )
+};
+
+root.render(<AppLayout/>)
