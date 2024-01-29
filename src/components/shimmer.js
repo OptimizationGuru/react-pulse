4eafacbf7ded48270f2f24ca8a5d960995fe1444
Shivam Tiwari <shivam.tiwari@yulu.bike>
2024-01-30 01:09:38 +0530

    shimmer ui fixed

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,24 +1,24 @@
 const Shimmer = () => {
 	return (
-		<div className="shimmer">
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"> </div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"> </div>
-			<div className="shimmer-card"></div>
-			<div className="shimmer-card"></div>
+		<div className="flex flex-wrap justify-center mt-[150px]">
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
+			<div className="text-center w-[250px] h-[480px] bg-gray-100 p-4 my-[10px] mx-[20px] right-0 left-0 rounded-lg"></div>
 		</div>
 	)
 }
